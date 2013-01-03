d7d98cd30bce4731322df3c61fa7a5699262e208
root <nanik-desktop>
2013-01-03 19:07:08 +1100

    add yaffs2 and cleanup binaries

--- fs/yaffs2/yaffs_mtdif.h
+++ fs/yaffs2/yaffs_mtdif.h
@@ -1,1 +1,23 @@
-/media/Seagate/Data/Android/yaffs2/master/yaffs_mtdif.h
+/*
+ * YAFFS: Yet another Flash File System . A NAND-flash specific file system.
+ *
+ * Copyright (C) 2002-2011 Aleph One Ltd.
+ *   for Toby Churchill Ltd and Brightstar Engineering
+ *
+ * Created by Charles Manning <charles@aleph1.co.uk>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 2.1 as
+ * published by the Free Software Foundation.
+ *
+ * Note: Only YAFFS headers are LGPL, YAFFS C code is covered by GPL.
+ */
+
+#ifndef __YAFFS_MTDIF_H__
+#define __YAFFS_MTDIF_H__
+
+#include "yaffs_guts.h"
+
+void yaffs_mtd_drv_install(struct yaffs_dev *dev);
+
+#endif
